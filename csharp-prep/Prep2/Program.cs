3d45e274fe1a347a99329e1f8e2fee395382ded7
whitej25 <jenavevewhite@gmail.com>
2023-04-25 14:57:28 -0600

    wrote code to determine grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int grade_percent = int.Parse(grade);
+
+
+        if (grade_percent >= 90) 
+        {
+            Console.WriteLine("Your grade is an A");
+        }
+        if (grade_percent >= 80 && grade_percent <= 89)
+        {
+            Console.WriteLine("Your grade is a B");
+        }
+        if (grade_percent >= 70 && grade_percent <= 79) 
+        {
+            Console.WriteLine("Your grade is a C");
+        }
+        if (grade_percent >= 60 && grade_percent <= 69) 
+        {
+            Console.WriteLine("Your grade is a D");
+        }
+        if (grade_percent <= 59) 
+        {
+            Console.WriteLine("Your grade is an F");
+        }
     }
 }
