68983ab85a0d8b42a16af2fd0926c9b1bd75a393
whitej25 <jenavevewhite@gmail.com>
2023-04-27 14:33:44 -0600

    Generated a magic number and has user guess the #.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random generator = new Random();
+        int magicNumber = generator.Next(1,101);
+
+        int userGuess = -1;
+        while (userGuess != magicNumber) {
+
+            Console.Write("Please guess a number between 1-100 ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess > magicNumber) {
+                Console.WriteLine("Lower");
+            }
+            if (userGuess < magicNumber) {
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("Congratulations! You Guessed the Magic Number!!");
+        Console.WriteLine();
+        
     }
+
 }
