2d45126fee12c9e68104a6b4066c101688130420
whitej25 <jenavevewhite@gmail.com>
2023-04-28 12:40:04 -0600

    Displays name and favorite number squared.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number. ");
+        int number = int.Parse(Console.ReadLine());
+
+        int squareNumber = number * number;
+        Console.WriteLine($"{name}, the square of your favorite number is {squareNumber}");
+         
     }
 }
