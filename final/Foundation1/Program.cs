0587e4f84a3202a6e2fd1eb43d11a514a36e589e
whitej25 <jenavevewhite@gmail.com>
2023-07-13 14:23:19 -0600

    Finished programs 1 & 2.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,30 +4,84 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("How to make spaghetti", "Joe B.", 10 );
+        Video video2 = new Video("How to change a tire", "Susan s.", 15 );
+        Video video3 = new Video("How to fold a shirt", "Sally Z.", 2 );
+        Video video4 = new Video("How to fix AC", "Carl. ",  25);
+
+        Comment comment1 = new Comment("Jerry", "WOAH");
+        Comment comment2 = new Comment("Jerry", "This is so helpful!");
+        Comment comment3 = new Comment("Sarah", "I already knew this");
+        Comment comment4 = new Comment("Tom", "Nice video!");
+        Comment comment5 = new Comment("Walter", "cool stuff");
+        Comment comment6 = new Comment("Rebecca ", "I loved this video :) ");
+        Comment comment7 = new Comment("Garret", "Inspirational. ");
+
+        video1.AddComment(comment1);
+        video1.AddComment(comment3);
+        video2.AddComment(comment3);
+        video3.AddComment(comment3);
+        video4.AddComment(comment3);
+        video2.AddComment(comment6);
+        video2.AddComment(comment4);
+        video3.AddComment(comment6);
+        video3.AddComment(comment1);
+        video4.AddComment(comment7);
+        video4.AddComment(comment2);
+        video4.AddComment(comment5);
+
+        List<Video> videos = new List<Video>{video1, video2, video3, video4};
+
+        foreach(Video video in videos){
+            System.Console.WriteLine($"Title: {video._title}");
+            System.Console.WriteLine($"Author: {video._author}");
+            System.Console.WriteLine($"Duration in minutes: {video._length}");
+            System.Console.WriteLine($"Number of Comments: {video.GetComments().Count()}");
+            
+            Console.WriteLine("Comments: ");
+            foreach(Comment comment in video.GetComments()){
+                System.Console.WriteLine($"- {comment._name}: {comment._text}");
+            }
+            
+        }
     }
 }
 
 class Video{
-    protected string _title;
-    protected string _author;
-    protected int _length;
+    public string _title;
+    public string _author;
+    public int _length;
+    public List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int length){
+        _title = title;
+        _author = author;
+        _length = length;
+
+    }
+
+    public void AddComment(Comment comment){
+        _comments.Add(comment);
+    }
 
     public int NumberComments(){
 
-        return ;
+        return _comments.Count();
+    }
+
+    public List<Comment> GetComments(){
+        return _comments;
     }
 
 }
 
 class Comment {
-    protected string _name;
-    private string _commentText;
+    public string _name;
+    public string _text;
 
-    Console.WriteLine("What is the name of the commentor?");
-    _name = Console.ReadLine();
-
-    Console.WriteLine("Please type your comment: ");
-    _commentText = Console.ReadLine();
+    public Comment(string name, string text){
+        _name = name;
+        _text = text;
+    }
     
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,6 +4,171 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+
+        Product product1 = new Product("Glasses","134673", 65 , 2);
+        Product product2 = new Product("Jeans", "187473", 75, 1);
+        Product product3 = new Product("Pencils", "628721", 1, 10);
+        Product product4 = new Product("Shoes", "783245", 60, 1);
+        Address address1 = new Address("786s 99w", "Orem", "Utah", "USA");
+        Address address2 = new Address("19n 10E", "Rexburg", "Idaho", "USA");
+        Address address3 = new Address("6s 4E", "Bountiful", "Utah", "USA");
+        Address address4 = new Address("20s 1w", "Logan", "Utah", "USA");
+        
+        Customer customer1 = new Customer("Frank", address2);
+        Customer customer2 = new Customer("Jena", address2);
+        Customer customer3 = new Customer("Linda", address2);
+        Customer customer4 = new Customer("Louise", address2);
+
+        var order0 = new List<Product>();
+            order0.Add(product1); 
+        
+        var jenaOrder = new List<Product>();
+            jenaOrder.Add(product2);
+
+        var lindaOrder = new List<Product>();
+            lindaOrder.Add(product3); 
+
+        var lousieOrder = new List<Product>();
+            lousieOrder.Add(product4);
+
+        Order order1 = new Order(order0, customer1, "Frank", "134673", 65, 2);
+        Order order2 = new Order(jenaOrder, customer2, "jena", "187473", 75, 1);
+        Order order3 = new Order(lindaOrder, customer3, "linda", "628721", 1, 10);
+        Order order4 = new Order(lousieOrder, customer4, "lousie", "783245", 60, 1);
+
+        DisplayOrder(order1);
+        DisplayOrder(order2);
+        DisplayOrder(order3);
+        DisplayOrder(order4);
+
+
+        static void DisplayOrder(Order order){
+            System.Console.WriteLine(order.GetPackingLabel());
+            System.Console.WriteLine(order.GetShippingLabel());
+            System.Console.WriteLine($"Total: {order.PriceCalculator()}");
+
+
+        }
+
+
     }
+}
+
+class Product {
+    protected string _name;
+    protected string _productID;
+    protected double _price;
+    protected int _quantity;
+
+    public Product(string name, string productID, double price, int quantity){
+        _name = name;
+        _productID = productID;
+        _price = price;
+        _quantity = quantity;
+    }
+    public string Name {get => _name; set => _name = value;}
+    public string ProductID {get => _productID; set => _productID = value;}
+    public double Price {get => _price; set => _price = value;}
+    public int Quantity{get => _quantity; set => _quantity = value;}
+
+    public double PriceCalculator(){
+        double _totalPrice = _price * _quantity;
+        return _totalPrice;
+    }
+
+}
+
+class Address{
+    protected string _streetAddress;
+    protected string _city;
+    protected string _state;
+    protected string _country; 
+
+    public Address(string streetAddress, string city, string state, string country){
+        _streetAddress = streetAddress;
+        _city = city;
+        _state = state;
+        _country = country;
+
+
+    }
+
+    public bool USAResident(){
+
+
+        if(_country.ToUpper() == "USA"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public string StreetAddress { get => _streetAddress; set => _streetAddress = value;} 
+    public string City {get => _city; set => _city = value;}
+    public string State {get => _state; set => _state = value;}
+    public string Country {get => _country; set => _country = value;}
+
+    public string GetAddress(){
+        string address = $"{_streetAddress}, {_city}, {_state}, {_country}";
+
+        return address;
+    }
+}
+
+class Customer{
+    protected string _name;
+    protected Address _address;
+
+    public Customer(string name, Address address){
+        _name = name;
+        _address = address;
+
+    }
+    public string Name {get => _name; set => _name = value;}
+    public Address Address{get => _address; set => _address = value;}
+    public bool USAResident(){
+        return _address.USAResident();
+    }
+
+}
+
+class Order: Product{
+    protected List<Product> _products;
+    protected Customer _customer;
+
+    public Order(List<Product> products, Customer customers, string name, string productID, int price, int quantity): base(name, productID, price, quantity){
+        _products = products;
+        _customer = customers;
+    }
+
+    public void AddProduct(Product product){
+        _products.Add(product);
+    }
+
+    public string GetPackingLabel(){
+        
+        string label = "Packing Label: ";
+        foreach(Product line in _products){
+            label += $"{line.Name}, ID: {line.ProductID}";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel(){
+        string list = $"Shipping Label: {_customer.Name} {_customer.Address.GetAddress()}";
+       
+        return list;
+    }
+    public double ShippingPrice(){
+        if(_customer.USAResident() == false){
+            return _price += 35;
+        }
+        else{
+            return _price += 5;
+        }      
+    }
+    public double TotalPrice(){
+        return _price += ShippingPrice();
+    }
+
 }
